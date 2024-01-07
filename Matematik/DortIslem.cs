76607698fee28a9465b26511bd8bf5a3d96c8aba
Can Kartal <can.krtl05@gmail.com>
2024-01-07 11:42:16 +0300

    Add project files.

---
+++ Matematik/DortIslem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik
+{
+    internal class DortIslem
+    {
+        public void Topla(double x, double y)
+        {
+            double toplam = x + y;
+            Console.WriteLine("Sonuç: " + toplam);
+        }
+        public void Cikar(double x, double y)
+        {
+            double toplam = x - y;
+            Console.WriteLine("Sonuç: " + toplam);
+        }
+        public void Carp(double x, double y)
+        {
+            double toplam = x * y;
+            Console.WriteLine("Sonuç: " + toplam);
+        }
+        public void Bol(double x, double y)
+        {
+            if(y > 0)
+            {
+                double toplam = x / y;
+                Console.WriteLine("Sonuç: " + toplam);
+            }
+            else
+            {
+                Console.WriteLine("0'dan büyük bir sayı giriniz");
+            }
+        }
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Matematik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+
+            dortIslem.Topla(2, 3);
+            dortIslem.Bol(0, 2);
+        }
+    }
+}
